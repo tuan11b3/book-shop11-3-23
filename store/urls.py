18ec7f0e6d6afaed7d7404e927a78d9a4e754972
Tuan <anhtuan11b3@gmail.com>
2023-03-02 17:46:18 +0700

    Started project.

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'store'
+urlpatterns = [
+    path('', views.store, name = 'store'),
+    path('cart/', views.cart, name = 'cart'),
+    path('checkout/', views.checkout, name = 'checkout'),
+    path('product/', views.product, name = 'product'),
+
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order")
+]
